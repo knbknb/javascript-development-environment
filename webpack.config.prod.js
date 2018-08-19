6bf76333201c5fea27dd29a6ed54a7c2a45ce11b
Knut Behrends <knb@tnp-online.de>
2018-08-19 13:33:16 +0200

    attempt to include manually changes from PR https://github.com/coryhouse/javascript-development-environment/pull/7

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,11 +3,15 @@ import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import WebpackMd5Hash from 'webpack-md5-hash';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
+//debug: true,
+//noInfo: false,
 
 export default {
-  debug: true,
+  mode: 'production',
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.json']
+  },
   devtool: 'source-map',
-  noInfo: false,
   entry: {
     vendor: path.resolve(__dirname, 'src/vendor'),
     main: path.resolve(__dirname, 'src/index')
@@ -18,19 +22,39 @@ export default {
     publicPath: '/',
     filename: '[name].[chunkhash].js'
   },
+  // Webpack 4 removed the commonsChunkPlugin. Use optimization.splitChunks instead.
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all'
+        }
+      }
+    }
+  },
   plugins: [
+    // Global loader configuration
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+      noInfo: true // set to false to see a list of every file being bundled.
+    }),
+
     // (separate css and js)
     // Generate an external css file with a hash in the filename
-    new ExtractTextPlugin('[name].[contenthash].css'),
-
+    //new ExtractTextPlugin('[name].[contenthash].css'),
+    new ExtractTextPlugin('[name].[md5:contenthash:hex:20].css'),
     // Hash the files using MD5 so that their names change when the content changes.
     new WebpackMd5Hash(),
 
     // Use CommonsChunkPlugin to create a separate bundle
     // of vendor libraries so that they're cached separately.
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
-    }),
+    // No longer used for Webpack 4. See optimization.splitChunks above instead.
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: 'vendor'
+    // }),
 
     // Create HTML file that includes reference to bundled JS.
     new HtmlWebpackPlugin({
@@ -51,18 +75,22 @@ export default {
       // Properties you define here are available in index.html
       // using htmlWebpackPlugin.options.varName
       trackJSToken: 'INSERT YOUR TOKEN HERE'
-    }),
+    })
 
     // Eliminate duplicate packages when generating bundle
-    new webpack.optimize.DedupePlugin(),
+    //new webpack.optimize.DedupePlugin()
 
+    // Code is automatically minified in prod mode as of Webpack 4, so removing this.
     // Minify JS
-    new webpack.optimize.UglifyJsPlugin()
+    //new webpack.optimize.UglifyJsPlugin()
   ],
   module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap') } // extra param is a hint to webpack
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader'] },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('css-loader?sourceMap')
+      } // extra param is a hint to webpack
     ]
   }
 };

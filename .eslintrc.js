603358ffd79e1230b718a85a6242af050998eba0
Knut Behrends <knb@tnp-online.de>
2018-09-17 23:49:57 +0200

    change eslint.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
   root: true,
   extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings"
+    'eslint:recommended',
+    // "eslint-config-airbnb-base",
+    'plugin:import/errors',
+    'plugin:import/warnings'
   ],
   parserOptions: {
     ecmaVersion: 7,
-    sourceType: "module"
+    sourceType: 'module'
   },
   env: {
     browser: true,
@@ -16,10 +17,10 @@ module.exports = {
     jquery: true
   },
   rules: {
-    "no-console": 1,
-    indent: ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "always"]
+    'no-console': 1,
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
   }
 };
